718c0448bd213d907fd101c8039c8767172758e5
trantuhao <tuhao99@gmail.com>
2020-12-16 21:10:33 +0700

    change host socketio host

--- src/components/Chat/useChat.js
+++ src/components/Chat/useChat.js
@@ -3,7 +3,7 @@ import socketIOClient from "socket.io-client";
 import {AuthenticationContext} from "../../providers/authenticationProvider";
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage"; // Name of the event
-const SOCKET_SERVER_URL = "http://localhost:3000";
+const SOCKET_SERVER_URL = process.env.REACT_APP_API_ENDPOINT;
 
 const useChat = (roomId) => {
     const [messages, setMessages] = useState([]); // Sent and received messages
