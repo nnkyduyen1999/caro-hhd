758bd748c63979272694277c9c721fcb7f2d0ff2
1712397-KD <1712397@student.hcmus.edu.vn>
2021-01-08 21:19:17 +0700

    receive new rooms socket (temp)

--- src/components/CreateRoom/createRoom.js
+++ src/components/CreateRoom/createRoom.js
@@ -3,20 +3,27 @@ import SportsEsportsIcon from "@material-ui/icons/SportsEsports";
 import { Button } from "@material-ui/core";
 import { AuthenticationContext } from "../../providers/authenticationProvider";
 import socket from "../../socket.io/socket.io";
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import TextField from '@material-ui/core/TextField';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
 
 const CreateRoom = (props) => {
-  const [newRoom, setNewRoom] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [timeStep, setTimeStep] = useState(null);
   const authenticationContext = useContext(AuthenticationContext);
-  const handleClick = () => {
-    console.log("click create please wait");
-    
-    // socket.emit("createRoom", authenticationContext.authenState.userInfo);
+
+  const handleClickCreate = (password, timeStep) => {
+    const socketData = {
+        password: password,
+        timeStep: timeStep,
+        userId: authenticationContext.authenState.userInfo._id
+    }
+    setOpen(false);
+
+    socket.emit("createRoom", socketData);
   };
 
   const [open, setOpen] = React.useState(false);
@@ -28,48 +35,69 @@ const CreateRoom = (props) => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const handleChangeTextField = (e) => {
+    switch (e.target.name) {
+      case "password":
+        setPassword(e.target.value);
+        break;
+      case "timeStep":
+        setTimeStep(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-   
     <div>
-     <Button
-      variant="outlined"
-      color="primary"
-      endIcon={<SportsEsportsIcon />}
-      onClick={handleClickOpen}
-      style={{marginTop: 20}}
-    >
-      Create Room
-    </Button>
-    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-      <DialogTitle id="form-dialog-title">Setting Your Room</DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="password"
-          label="Room password"
-          type="text"
-          fullWidth
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="timeStep"
-          label="Time for each step"
-          type="number"
-          fullWidth
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="primary">
-          Cancel
-        </Button>
-        <Button variant="contained" onClick={handleClose} color="primary">
-          Subscribe
-        </Button>
-      </DialogActions>
-    </Dialog>
-  </div>
+      <Button
+        variant="outlined"
+        color="primary"
+        endIcon={<SportsEsportsIcon />}
+        onClick={handleClickOpen}
+        style={{ marginTop: 20 }}
+      >
+        Create Room
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Setting Your Room</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="password"
+            name="password"
+            label="Room password"
+            type="text"
+            onChange={handleChangeTextField}
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="timeStep"
+            name="timeStep"
+            label="Time for each step"
+            type="number"
+            onChange={handleChangeTextField}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button variant="contained" onClick={() => handleClickCreate(password, timeStep)} color="primary">
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
--- src/components/FinishedBoards/finishedBoards.js
+++ src/components/FinishedBoards/finishedBoards.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import socket from "../../socket.io/socket.io";
 
 function FinishedBoards() {
+    const [listRoom, setListRoom] = useState([]);
+    useEffect(() => {
+        socket.on("newRoomCreated", listRoom => {
+            setListRoom([...listRoom]);
+        });
+        console.log("list room", listRoom);
+    }, [listRoom]);
     return (
         <div>
             finishedBoards
