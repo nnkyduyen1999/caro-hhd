ee21b95ccc812efbe0632fbbf1570526cde12286
1712397-KD <1712397@student.hcmus.edu.vn>
2021-01-12 00:53:02 +0700

    sent email forget password

--- src/components/ForgetPassword/forgetPassword.js
+++ src/components/ForgetPassword/forgetPassword.js
@@ -1,55 +1,57 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Alert from "@material-ui/lab/Alert";
+import axios from "axios";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         HHD Team
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100vh',
+    height: "100vh",
   },
   image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
     backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
   },
   paper: {
     margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -59,12 +61,48 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ForgetPassword() {
   const classes = useStyles();
+  const [msg, setMsg] = useState(``);
+  const [isErr, setIsErr] = useState(false);
+  const [email, setEmail] = useState(``);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (email === `` || !email) {
+      setIsErr(true);
+      setMsg("Please add your email.");
+      return;
+    }
+
+    axios.post(`${process.env.REACT_APP_API_ENDPOINT}/forget-password`, {
+        email: email
+    }).then(res => {
+        if (res.status === 200) {
+            setIsErr(false);
+            setMsg(res.data.message);
+        }
+    }).catch(err => {
+        setIsErr(true);
+        setMsg(err.response.data.message);
+    })
+  };
+
+  const renderAlert = (msg, isErr) => {
+    if (!msg) {
+      return <></>;
+    } else {
+      const status = isErr ? "error" : "success";
+      return <Alert severity={status}>{msg}</Alert>;
+    }
+  };
+
 
   return (
     <Grid container component="main" className={classes.root}>
       <CssBaseline />
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        {renderAlert(msg, isErr)}
         <div className={classes.paper}>
           <Avatar className={classes.avatar}>
             <LockOutlinedIcon />
@@ -72,7 +110,11 @@ export default function ForgetPassword() {
           <Typography component="h1" variant="h5">
             Get your password
           </Typography>
-          <form className={classes.form} noValidate>
+          <form
+            className={classes.form}
+            noValidate
+            onSubmit={(e) => handleFormSubmit(e)}
+          >
             <TextField
               variant="outlined"
               margin="normal"
@@ -83,8 +125,9 @@ export default function ForgetPassword() {
               name="email"
               autoComplete="email"
               autoFocus
+              onChange={(e) => setEmail(e.target.value)}
             />
-            
+
             <Button
               type="submit"
               fullWidth
